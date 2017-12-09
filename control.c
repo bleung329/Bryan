8ae3fe5b2a4d7d305c36c27dc8c3729c78784efd
Ryan Siu <rsiu@stuy.edu>
2017-12-09 16:38:58 -0500

    Add -c tag

---
+++ control.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <errno.h>
+
+#define KEY 1024
+
+int main( int argc, char *argv[] ) {
+
+  union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+  } data;
+
+  if (argc != 1) {
+    printf("Invalid arguments.\n");
+    return 0;
+  }
+
+  if (strcmp(argv[1], "-c")) {
+    int sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+    if (sd == -1) {
+      printf("Error creating semaphore: %s", strerror(errno));
+      return 0;
+    }
+    data.val = 1;
+    semctl(sd, 0, SETVAL, data.val);
+    printf("[%d] Created semaphore!", sd);
+  }
+
+  else if (strcmp(argv[1], "-v")) {
+
+  }
+
+  else if (strcmp(argv[1], "-r")) {
+
+  }
+
+  return 0;
+}
